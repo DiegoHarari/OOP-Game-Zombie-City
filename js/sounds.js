998793114a77bd988c909997d79cd91353a29188
DiegoHarari <diehara@gmail.com>
2019-01-26 21:13:25 -0300

    first commit

---
+++ js/sounds.js
@@ -0,0 +1,16 @@
+let sound = new Howl({
+    src: ['audios/choque.mp3']
+  });
+
+let soundTwo = new Howl({
+    src: ['audios/theEnd.mp3']
+  });
+
+  let soundThree = new Howl({
+    src: ['audios/background.mp3']
+  })
+  
+
+
+
+
